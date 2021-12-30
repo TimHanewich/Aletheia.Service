44584733bfaea79b87bb8312054de7630118c5c4
Tim Hanewich <tahanewich@live.com>
2021-12-29 20:27:39 -0500

    Finished latest transactions

--- src/InsiderTrading/Extensions.cs
+++ src/InsiderTrading/Extensions.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using TimHanewich.Toolkit.Web;
 
 namespace Aletheia.Service.InsiderTrading
 {
@@ -72,6 +73,85 @@ namespace Aletheia.Service.InsiderTrading
             return ToReturn.ToArray();
         }
 
+        public static async Task<SecurityTransactionHolding[]> LatestTransactions(this AletheiaService service, LatestTransactionsRequest request)
+        {
+
+            #region "Construct the URL"
+
+
+            UrlConstructor urlcon = new UrlConstructor();
+            urlcon.Base = "https://api.aletheiaapi.com/LatestTransactions";
+
+            //Issuer
+            if (request.Issuer != null)
+            {
+                if (request.Issuer != "")
+                {
+                    urlcon.AddQueryParameter(new UrlQueryParameter("issuer", request.Issuer));
+                }
+            }
+
+            //Owner
+            if (request.Owner != null)
+            {
+                if (request.Owner != "")
+                {
+                    urlcon.AddQueryParameter(new UrlQueryParameter("owner", request.Owner));
+                }
+            }
+
+            //Top 
+            if (request.Top.HasValue)
+            {
+                urlcon.AddQueryParameter(new UrlQueryParameter("top", request.Top.Value.ToString()));
+            }
+
+            //Before
+            if (request.Before.HasValue)
+            {
+                string DateFormatted = request.Before.Value.Year.ToString("0000") + request.Before.Value.Month.ToString("00") + request.Before.Value.Day.ToString("00");
+                urlcon.AddQueryParameter(new UrlQueryParameter("before", DateFormatted));
+            }
+            
+            //Security type
+            if (request.SecurityType.HasValue)
+            {
+                urlcon.AddQueryParameter(new UrlQueryParameter("SecurityType", Convert.ToInt32(request.SecurityType.Value).ToString()));
+            }
+
+            //Transaction type
+            if (request.TransactionType.HasValue)
+            {
+                urlcon.AddQueryParameter(new UrlQueryParameter("TransactionType", Convert.ToInt32(request.TransactionType.Value).ToString()));
+            }
+
+            //Cascade
+            if (request.Cascade.HasValue)
+            {
+                urlcon.AddQueryParameter(new UrlQueryParameter("cascade", request.Cascade.Value.ToString().ToLower()));
+            }
+
+            string URL = urlcon.ToString();
+
+            #endregion
+        
+            //Make the call
+            HttpRequestMessage req = service.PrepareHttpRequestMessage();
+            req.Method = HttpMethod.Get;
+            req.RequestUri = new Uri(URL);
+            HttpClient hc = new HttpClient();
+            HttpResponseMessage resp = await hc.SendAsync(req);
+            if (resp.StatusCode != HttpStatusCode.OK)
+            {
+                string content = await resp.Content.ReadAsStringAsync();
+                throw new Exception("Latest Transactions request to Aletheia returned code " + resp.StatusCode.ToString() + ". Msg: " + content);
+            }
+
+            //Parse body
+            string body = await resp.Content.ReadAsStringAsync();
+            SecurityTransactionHolding[] ToReturn = JsonConvert.DeserializeObject<SecurityTransactionHolding[]>(body);
+            return ToReturn;
+        }
         
     }
 }
--- src/InsiderTrading/LatestTransactionsRequest.cs
+++ src/InsiderTrading/LatestTransactionsRequest.cs
@@ -10,5 +10,6 @@ namespace Aletheia.Service.InsiderTrading
         public DateTime? Before {get; set;}
         public SecurityType? SecurityType {get; set;}
         public TransactionType? TransactionType {get; set;}
+        public bool? Cascade {get; set;}
     }
 }
